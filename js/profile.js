585033a3870fffd3c0e372d1c3f5b3f5e812ce36
Devan <DevanJanssen@gmail.com>
2024-01-17 14:39:36 +0100

    m

--- js/profile.js
+++ js/profile.js
@@ -3,19 +3,13 @@ function getProfile() {
     var apiUrl = APIaddress() + 'User/GetProfile?auth_key=' + encodeURIComponent(authKey);
 
     fetch(apiUrl)
-        .then(response => response.json()) // Assuming the response is in JSON format
+        .then(response => response.json())
         .then(data => {
             if (data.length > 0) {
-                // Update profile name
+                // Update html data based on json data
                 document.getElementById('profileName').innerText = data[0]['user_displayname'] || 'No Display Name';
-
-                // Update profile email
                 document.getElementById('profileEmail').innerText = data[0]['user_email'];
-
-                // Update profile description
                 document.getElementById('profileDescription').innerText = data[0]['user_bio'];
-
-                // You can add more updates for other elements as needed
 
             } else {
                 console.log("Empty data received");
