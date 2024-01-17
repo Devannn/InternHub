3f82731205684d7e5b70d1f96bcf86718a51d715
Devan Janssen <devanjanssen@gmail.com>
2024-01-17 13:44:55 +0100

    testing profile

--- js/profile.js
+++ js/profile.js
@@ -1,18 +1,31 @@
 function getProfile() {
     var authKey = GetAuthKey();
-
     var apiUrl = APIaddress() + 'User/GetProfile?auth_key=' + encodeURIComponent(authKey);
 
     fetch(apiUrl)
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
-            if (data == 0) {
+            if (data === 0) {
                 console.log("0 received");
             } else {
-                console.log(data);
+                // Update the HTML elements with the received data
+                updateProfileUI(data);
             }
         })
         .catch((error) => {
             console.log("error");
         });
+}
+
+// Function to update the HTML elements with profile data
+function updateProfileUI(profileData) {
+    // Assuming profileData is an object with properties like name, description, etc.
+
+    // Update the name
+    document.getElementById('profileName').textContent = profileData.name;
+
+    // Update the description
+    document.getElementById('profileDescription').textContent = profileData.description;
+
+    // You can continue updating other elements as needed
 }
