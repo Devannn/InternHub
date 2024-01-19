244173ef5dca4f788eec39ba7960f7018f12bc51
Devan Janssen <devanjanssen@gmail.com>
2024-01-19 10:22:11 +0100

    added internships to company page

--- company.php
+++ company.php
@@ -152,49 +152,99 @@ include 'inc/checkAuthKey.php';
                             <div class="card card-company-bio">
                                 <div class="card-body">
                                     <h3>Internships</h3>
-                                    <table class="table table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">Title</th>
-                                                <th scope="col">Tags</th>
-                                                <th scope="col"></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>HBO Graduation Assignment: Make a website for a pizza company</td>
-                                                <td>Laravel</td>
-                                                <td>
-                                                    <form action="assignment.php">
-                                                        <input type="submit" class="btn btn-outline-primary btn-assignment-link" value="Details" />
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>HBO Graduation Assignment: Make a website for a pizza company</td>
-                                                <td>React</td>
-                                                <td>
-                                                    <form action="assignment.php">
-                                                        <input type="submit" class="btn btn-outline-primary btn-assignment-link" value="Details" />
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>HBO Graduation Assignment: Make a website for a pizza company</td>
-                                                <td>Vue.JS</td>
-                                                <td>
-                                                    <form action="assignment.php">
-                                                        <input type="submit" class="btn btn-outline-primary btn-assignment-link" value="Details" />
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                                    <div class="col">
+                                        <div class="card companies-card">
+                                            <div class="card-body">
+                                                <a href="company.php" class="company-link">
+                                                    <div class="row">
+                                                        <div class="col-2">
+                                                            <div class="companies-image">
+                                                                <img src="/InternHub/img/roc-logo.png" alt="">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col">
+                                                            <div class="row">
+                                                                <div class="col text-bold">Roc Ter AA</div>
+                                                                <div class="col text-bold d-flex justify-content-end">Noord-Brabant</div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col">Web Developer</div>
+                                                                <div class="col d-flex justify-content-end">
+                                                                    <div class="rating">
+                                                                        <div class="rating rating-text">3,7</div>
+                                                                        <div class="rating rating-star">
+                                                                            <span class="fa fa-star checked"></span>
+                                                                        </div>
+                                                                        <div class="rating rating-text-small">(4)</div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <div class="py-2"></div>
+                                                <div class="row flex-nowrap overflow-auto">
+                                                    <div class="tag d-inline-block">Laravel</div>
+                                                    <div class="tag d-inline-block">Vue.JS</div>
+                                                    <div class="tag d-inline-block">Bootstrap</div>
+                                                    <div class="tag d-inline-block">Next.JS</div>
+                                                    <div class="tag d-inline-block">React</div>
+                                                    <div class="tag d-inline-block">Laravel</div>
+                                                </div>
+                                                <div class="companies-text-right col-text-small">
+                                                    18-12-2023
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card companies-card">
+                                            <div class="card-body">
+                                                <a href="company.php" class="company-link">
+                                                    <div class="row">
+                                                        <div class="col-2">
+                                                            <div class="companies-image">
+                                                                <img src="/InternHub/img/roc-logo.png" alt="">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col">
+                                                            <div class="row">
+                                                                <div class="col text-bold">Roc Ter AA</div>
+                                                                <div class="col text-bold d-flex justify-content-end">Noord-Brabant</div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col">Web Developer</div>
+                                                                <div class="col d-flex justify-content-end">
+                                                                    <div class="rating">
+                                                                        <div class="rating rating-text">3,7</div>
+                                                                        <div class="rating rating-star">
+                                                                            <span class="fa fa-star checked"></span>
+                                                                        </div>
+                                                                        <div class="rating rating-text-small">(4)</div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <div class="py-2"></div>
+                                                <div class="row flex-nowrap overflow-auto">
+                                                    <div class="tag d-inline-block">Laravel</div>
+                                                    <div class="tag d-inline-block">Vue.JS</div>
+                                                    <div class="tag d-inline-block">Bootstrap</div>
+                                                    <div class="tag d-inline-block">Next.JS</div>
+                                                    <div class="tag d-inline-block">React</div>
+                                                    <div class="tag d-inline-block">Laravel</div>
+                                                </div>
+                                                <div class="companies-text-right col-text-small">
+                                                    18-12-2023
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <!-- Internships -->
-                        
+
                         <!-- Assigments -->
                         <div class="tab-pane fade" id="four">
                             <div class="card card-company-bio">
