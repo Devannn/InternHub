abe8e0a22113f28c2f77c974267347121eacd873
DW <ps218325@gmail.com>
2024-01-19 15:04:55 +0100

    update: get the displayname when chatting.

--- messages.php
+++ messages.php
@@ -107,7 +107,7 @@ include 'inc/checkAuthKey.php';
                 <div class="col-md-8 order-md-2">
                     <div class="card">
                         <div class="card-header">
-                            <span id="receiver">NAME</span>
+                            <span id="receiver"></span>
                         </div>
                         <div class="card-body">
                             <div id="chat-container"></div>
@@ -129,6 +129,22 @@ include 'inc/checkAuthKey.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 <script>
+(async function() {
+	try {
+		const authKey = GetAuthKey();
+		const receiverName = await getDisplayNameFromUserID(authKey, getParameterByName('i'));
+		
+		if (receiverName == "0") 
+		{
+			receiverName = "No Display Name"; 
+		}
+		
+		document.getElementById('receiver').innerHTML = receiverName;
+	} catch (error) {
+		console.error('Error updating receiver:', error);
+	}
+})();
+
     const socket = new WebSocket(WSaddress());
     var authKey = GetAuthKey();
 	var user_id;	
