13b2f8313d83389971bf87ff04fced00903dd360
DW <ps218325@gmail.com>
2024-01-19 15:18:10 +0100

    update: remove messagebox when no user is selected

--- messages.php
+++ messages.php
@@ -106,16 +106,20 @@ include 'inc/checkAuthKey.php';
                 <!-- Message Box -->
                 <div class="col-md-8 order-md-2">
                     <div class="card">
-                        <div class="card-header">
-                            <span id="receiver">No Display Name</span>
-                        </div>
-                        <div class="card-body">
-                            <div id="chat-container"></div>
-                        </div>
-                        <div class="card-footer">
-                            <input type="text" id="messageInput" placeholder="Type your message">
-                            <button onclick="sendMessage(getParameterByName('i'), document.getElementById('messageInput'))">Send</button>
-                        </div>
+                        
+						<div id="messagebox">
+							<div class="card-header">
+								<span id="receiver">No Display Name</span>
+							</div>
+							<div class="card-body">
+								<div id="chat-container"></div>
+							</div>
+							<div class="card-footer">
+								<input type="text" id="messageInput" placeholder="Type your message">
+								<button onclick="sendMessage(getParameterByName('i'), document.getElementById('messageInput'))">Send</button>
+							</div>
+						</div>
+						
                     </div>
                 </div>
                 <!-- Message Box -->
@@ -139,6 +143,15 @@ include 'inc/checkAuthKey.php';
 		}
 
 		document.getElementById('receiver').innerHTML = receiverName;
+		
+		// Check if receiverName is 0, and hide the textbox accordingly
+        const messageInput = document.getElementById('messagebox');
+        if (receiverName == "0") {
+            messageInput.style.display = 'none';
+        } else {
+            messageInput.style.display = 'block';
+        }
+		
 	} catch (error) {
 		console.error('Error updating receiver:', error);
 	}
